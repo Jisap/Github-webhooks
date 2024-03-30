685d614f84535405310bb3ecfdb7aee6a8166604
Jorge Matos <jisapjisap@gmail.com>
2024-03-30 17:21:51 +0100

    imagenes de respuesta en Discord

--- src/presentation/services/discord.service.ts
+++ src/presentation/services/discord.service.ts
@@ -13,6 +13,11 @@ export class DiscordService {
   
     const body = {
       content: message,
+      embeds: [
+        {
+          image: { url: 'https://plus.unsplash.com/premium_photo-1678565869434-c81195861939?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
+        }
+      ]
     }
 
     const resp = await fetch( this.discordWebhookUrl, {
