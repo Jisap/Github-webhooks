84992e5fefb0b951072cc8582a10a135cc2e7a40
Jorge Matos <jisapjisap@gmail.com>
2024-04-01 17:15:32 +0200

    Seguridad de webhooks

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { envs } from './config';
 import { GithubController } from './presentation/github/controller';
+import { GithubSha256Middleware } from './presentation/middlewares/github-sha256.middleware';
 
 (() => {
   main();
@@ -13,6 +14,8 @@ function main () {
   const controller = new GithubController();
 
   app.use( express.json() );
+
+  app.use(GithubSha256Middleware.verifySignature);
 
   app.post('/api/github',  controller.webhookHandler)
 
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,4 +5,5 @@ export const envs = {
 
   PORT: get('PORT').required().asPortNumber(),
   DISCORD_WEBHOOK_URL: get('DISCORD_WEBHOOK_URL').required().asString(),
+  SECRET_TOKEN: get('SECRET_TOKEN').required().asString(),
 }
